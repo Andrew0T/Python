cfda3588877ac4440c55361fdc88c8dd7cafdcd4
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-05-23 21:05:29 +0200

    Create loops.py

---
+++ Exercise_1.3/1.3-practise_task_3/loops.py
@@ -0,0 +1,14 @@
+# for and while 
+
+task_3 = 10
+
+for i in range(10,60,10):
+  print(task_3)
+  task_3 += 10
+  continue
+print("And we're done!")
+
+while task_3 < 60:
+  print(task_3)
+  task_3 += 10
+print("And we're done!")
